2eca61d14435f26266f9e34cb03e51e1049983d8
Leweuehu <leweyehuyirsaw55@gmail.com>
2026-03-03 22:06:35 +0300

    Moved certeficate file final

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -6,17 +6,17 @@ const Certifications = () => {
       <h2 className="section-title">Certifications</h2>
 
       <ul className="cert-list">
-        <li>✔ PythonProgramming Certification</li>
-        <li>✔ Microsoft AI Fundamentals</li>
-        <li>✔ React / Node.js Professional Courses</li>
-        <li>✔ Cybersecurity Fundamentals</li>
-        <li>✔ Django Web Development Certification</li>
-        <li>✔ Android developments</li>
-        <li>✔ Cybersecurity Fundamentals</li>
+        <li>PythonProgramming Certification</li>
+        <li>Microsoft AI Fundamentals</li>
+        <li>React / Node.js Professional Courses</li>
+        <li>Cybersecurity Fundamentals</li>
+        <li>Django Web Development Certification</li>
+        <li>Android developments</li>
+        <li>Cybersecurity Fundamentals</li>
         <li>FreeCodeCamp(Data Analysis)</li>
-        <li>✔Microsoft 365</li>
-        <li>✔Leadership Online</li>
-        <li>✔edX</li>
+        <li>Microsoft 365</li>
+        <li>Leadership Online</li>
+        <li>edX</li>
       </ul>
     </section>
   );
